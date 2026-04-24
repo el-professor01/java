11d925c813094991b1eb463c238782753bd6175b
el-professor01 <aljomathegreatone@gmail.com>
2026-04-24 15:33:14 +0300

    Add files via upload

---
+++ masinde.java
@@ -0,0 +1,8 @@
+public class masinde {
+    public static void main(String[] args) {
+        System.out.println("Jina langu ni ______");
+        System.out.println("Nasoma Java");
+        System.out.println("Nitakuwa programmer mzuri");
+    }
+}
+
